11219445f40c743775a44224c4f0c822d539641b
Simon <s.lindgren@hotmail.se>
2021-10-17 11:03:28 +0200

    End of section 3

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 ﻿
 using Core.Entities;
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -14,25 +15,43 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-        private readonly StoreContext _context;
-        public ProductsController(StoreContext context)
+        private readonly IProductRepository _repo;
+        public ProductsController(IProductRepository repo)
         {
-            _context = context; 
+            _repo = repo;
         }
 
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            var products = await _context.Products.ToListAsync();
+            var products = await _repo.GetProductsAsync();
 
             return Ok(products);
         }
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            var product = await _context.Products.FindAsync(id);
+            return await _repo.GetProductByIdAsync(id);
+        }
 
-            return Ok(product);
+        [HttpGet("Brands")]
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
+        {
+            //Old way of returning products.
+            //var productBrand = await _repo.GetProductBrandAsync();
+            //return Ok(productBrand);
+
+            return Ok(await _repo.GetProductBrandsAsync());
+        }
+
+        [HttpGet("Types")]
+        public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
+        {
+            //Old way of returning products. 
+            //var productTypes = await _repo.GetProductTypeAsync();
+            //return  Ok(productTypes);
+
+            return Ok(await _repo.GetProductTypesAsync());
         }
     }
 }
