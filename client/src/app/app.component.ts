590d6ff15b98d38282603eb983cc39f341489950
Simon <s.lindgren@hotmail.se>
2022-01-22 21:49:18 +0100

    Added functionality to item cart and localstorage persistance with items in cart.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BasketService } from './basket/basket.service';
 
 
 @Component({
@@ -11,9 +12,17 @@ export class AppComponent implements OnInit {
   title = 'PlopStore';
   
 
-  constructor() {}
+  constructor(private basketService: BasketService) {}
 
   ngOnInit(): void {
+    const basketId = localStorage.getItem('basket_id');
+    if(basketId) {
+      this.basketService.getBasket(basketId).subscribe(() => {
+        console.log('initilized basket');
+      }, error => {
+        console.log(error);
+      });
+    }
   }
 
 }
