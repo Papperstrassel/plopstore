d3e2e409258224e76aac5f06b9379edfcc20362f
Simon <s.lindgren@hotmail.se>
2022-02-16 22:32:46 +0100

    Addressed modelstate error in login and register form and added async Validation for registering.

--- client/src/app/account/account.service.ts
+++ client/src/app/account/account.service.ts
@@ -51,6 +51,7 @@ export class AccountService {
       map((user: IUser) => {
         if(user) {
           localStorage.setItem('token', user.token);
+          this.currentUserSource.next(user);
         }
       })
     );
@@ -63,6 +64,6 @@ export class AccountService {
   }
 
   checkEmailExists(email: string) {
-    return this.http.get(this.baseUrl + '/account/emailexists?email=' + email);
+    return this.http.get(this.baseUrl + 'account/emailexists?email=' + email);
   }
 }
