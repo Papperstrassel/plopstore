60ca69f730c55b58d7fba283942bda0dd98531f7
Simon <s.lindgren@hotmail.se>
2022-04-12 18:55:31 +0200

    Added Card validation support for client from Stripe and completed stepper functionality for checkout process.

--- client/src/app/checkout/checkout-payment/checkout-payment.component.ts
+++ client/src/app/checkout/checkout-payment/checkout-payment.component.ts
@@ -24,6 +24,7 @@ export class CheckoutPaymentComponent implements AfterViewInit, OnDestroy {
   cardExpiry: any;
   cardCvc: any;
   cardErrors: any;
+  cardHandler = this.onChange.bind(this);
 
   constructor(private basketService: BasketService, private checkoutService: CheckoutService,
     private toastr: ToastrService, private router: Router) { }
@@ -31,16 +32,21 @@ export class CheckoutPaymentComponent implements AfterViewInit, OnDestroy {
   ngAfterViewInit(){
 
     this.stripe = Stripe('pk_test_51KkuclHkwpAdPuoJiyqCalBcW1s2eMUL2ne8lAyM21EZwoiVuVTYxVB0uvG3WBrhLYIPY0u9s8Drr2a9AUut7O2w004VyEw6pZ');
+
+    // Mount stripe element in our "nativeElement" our html element.
     const elements = this.stripe.elements();
 
     this.cardNumber = elements.create('cardNumber');
-    this.cardNumber.mount(this.cardNumberElement.nativeElement); // Mount stripe element in our "nativeElement" our html element.
+    this.cardNumber.mount(this.cardNumberElement.nativeElement);
+    this.cardNumber.addEventListener('change', this.cardHandler);
 
     this.cardExpiry = elements.create('cardExpiry');
-    this.cardExpiry.mount(this.cardExpiryElement.nativeElement); // Mount stripe element in our "nativeElement" our html element.
+    this.cardExpiry.mount(this.cardExpiryElement.nativeElement);
+    this.cardExpiry.addEventListener('change', this.cardHandler);
 
     this.cardCvc = elements.create('cardCvc');
-    this.cardCvc.mount(this.cardCvcElement.nativeElement); // Mount stripe element in our "nativeElement" our html element.
+    this.cardCvc.mount(this.cardCvcElement.nativeElement);
+    this.cardCvc.addEventListener('change', this.cardHandler);
 
   }
 
@@ -49,6 +55,15 @@ export class CheckoutPaymentComponent implements AfterViewInit, OnDestroy {
     this.cardNumber.destroy();
     this.cardExpiry.destroy();
     this.cardCvc.destroy();
+  }
+
+  onChange({error}) {
+    if (error) {
+      this.cardErrors = error.message; //Card errors is coming from Stripe, they are doing the validation
+    } else {
+      this.cardErrors = null;
+    }
+
   }
 
   submittOrder() {
