fb045111249c946a4d8724b454cf5fd9b6d8b0ba
Simon <s.lindgren@hotmail.se>
2022-04-13 20:46:39 +0200

    Added API endpoint for Stripe events.

--- API/Controllers/PaymentsController.cs
+++ API/Controllers/PaymentsController.cs
@@ -1,19 +1,26 @@
+using System.IO;
 using System.Threading.Tasks;
 using API.Errors;
 using Core.Entities;
 using Core.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Stripe;
+using Order = Core.Entities.OrderAggregate.Order; //Removes ambigouity on what order object we're using. We use our order object and not the one coming from Stripe class. 
 
 namespace API.Controllers
 {
     public class PaymentsController : BaseApiController
     {
         private readonly IPaymentService _paymentService;
+        private readonly ILogger<PaymentsController> _logger;
+        private const string WhSecret = ""; // We're going to get "something" from stripe telling us we can trust it. 
 
-        public PaymentsController(IPaymentService paymentService)
+        public PaymentsController(IPaymentService paymentService, ILogger<PaymentsController> logger)
         {
             _paymentService = paymentService;
+            _logger = logger;
         }
 
         [Authorize]
@@ -26,5 +33,37 @@ namespace API.Controllers
 
            return basket;
         }
+
+        [HttpPost("webhook")] //At this point we're confirming payment from Stripe so we need to make sure its secure since its open to anyone to request this api. 
+        public async Task<ActionResult> StripeWebhook()
+        {
+            // We need to read whats coming from the Stripe webhook to this API endpoint. 
+            var json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync(); 
+
+            //pass in the WbSecret to make sure this is actually coming from Stripe, so we can trust the stripe event.
+            var stripeEvent = EventUtility.ConstructEvent(json, Request.Headers["Stripe-Signature"],
+                WhSecret);
+
+            PaymentIntent intent;
+            Order order; //Order class from Core entities that we have created. 
+
+            switch(stripeEvent.Type)
+            {
+                case "payment_intent.succeeded":
+                    intent = (PaymentIntent)stripeEvent.Data.Object;
+                    _logger.LogInformation("Payment Succeeded: ", intent.Id);
+                    // TODO: update order with new status
+                    break;
+                case "payment_intent.payment_failed":
+                    intent = (PaymentIntent)stripeEvent.Data.Object;
+                    _logger.LogInformation("Payment Failed: ", intent.Id);
+                    // TODO: update order with new status
+                    break;
+            }
+            //We need to notify Stripe that we have recieved their events, and we do this by sending a new emptyResult
+            //In production Stripe will keep resending these events for up to 3 days  if a response from us is not recieved.  
+            return new EmptyResult();
+        }
+
     }
 }
