3422f7068eb1ada482ca6553fc1b1f69f3f187a3
Simon <s.lindgren@hotmail.se>
2022-05-16 23:15:40 +0200

    Redesigned navbar and home page.

--- client/src/app/core/nav-bar/nav-bar.component.ts
+++ client/src/app/core/nav-bar/nav-bar.component.ts
@@ -1,18 +1,50 @@
+import { animate, state, style, transition, trigger, useAnimation } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AccountService } from 'src/app/account/account.service';
 import { BasketService } from 'src/app/basket/basket.service';
 import { IBasket } from 'src/app/shared/models/basket';
 import { IUser } from 'src/app/shared/models/user';
+import { SidebarCloseAnimation, SidebarOpenAnimation } from './nav-bar-animations';
+
+
+const animationParams = {
+  menuWidth: "250px",
+  animationStyle: "500ms ease"
+};
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.scss']
+  styleUrls: ['./nav-bar.component.scss'],
+  animations: [
+    trigger("sideMenu", [
+      transition(":enter", [
+        useAnimation(SidebarOpenAnimation, {
+          params: {
+            ...animationParams
+          }
+        })
+      ]),
+      transition(":leave", [
+        useAnimation(SidebarCloseAnimation, {
+          params: {
+            ...animationParams
+          }
+        })
+      ])
+    ])
+  ]
+
+
+
 })
 export class NavBarComponent implements OnInit {
   basket$: Observable<IBasket>;
   currentUser$: Observable<IUser>;
+
+
+  isOpen = false;
 
   constructor(private basketService: BasketService, private accountService: AccountService) { }
 
@@ -20,6 +52,8 @@ export class NavBarComponent implements OnInit {
     this.basket$ = this.basketService.basket$;
     this.currentUser$ = this.accountService.currentUser$;
   }
+
+
 
   logout() {
     this.accountService.logout();
